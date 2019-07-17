26becfc31626a596a918dd7ea199340e50f7362d
Shrikant Vishwakarma <zeeroiq@gmail.com>
2019-07-17 22:37:56 +0530

    Initial project add

---
+++ src/main/java/com/shri/spring/boot/SpringBootPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.shri.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPetClinicApplication.class, args);
+    }
+
+}
