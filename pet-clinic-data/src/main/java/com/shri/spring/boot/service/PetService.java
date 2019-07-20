8937a59f110f480a55e60b5395f0102206464a05
Shrikant Vishwakarma <zeeroiq@gmail.com>
2019-07-21 01:23:52 +0530

    Implemented interfaces for services. Closes #5

--- pet-clinic-data/src/main/java/com/shri/spring/boot/service/PetService.java
+++ pet-clinic-data/src/main/java/com/shri/spring/boot/service/PetService.java
@@ -2,12 +2,6 @@ package com.shri.spring.boot.service;
 
 import com.shri.spring.boot.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-    Pet save(Pet owner);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 
 }
--- pet-clinic-data/src/main/java/com/shri/spring/boot/service/VetService.java
+++ pet-clinic-data/src/main/java/com/shri/spring/boot/service/VetService.java
@@ -1,13 +1,7 @@
 package com.shri.spring.boot.service;
 
-import com.shri.spring.boot.model.Owner;
 import com.shri.spring.boot.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Owner owner);
-    Set<Vet> findAll();
 }
