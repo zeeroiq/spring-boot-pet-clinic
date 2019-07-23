8b256c6c2061822465c37c660a8dc2a6e24c94bd
Shrikant Vishwakarma <zeeroiq@gmail.com>
2019-07-23 23:38:48 +0530

    Updated BaseEntity with JPA annotations

--- pet-clinic-data/src/main/java/com/shri/spring/boot/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/shri/spring/boot/model/BaseEntity.java
@@ -2,10 +2,17 @@ package com.shri.spring.boot.model;
 
 import lombok.Data;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 @Data
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 }
